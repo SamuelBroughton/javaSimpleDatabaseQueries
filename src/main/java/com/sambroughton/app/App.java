43e70c4821f1b67701bf417de1f8663f4fa5baab
sambroughton <sjbroughton1@sheffield.ac.uk>
2018-02-28 01:48:47 +0000

    SQL queries completed with no user interaction

--- src/main/java/com/sambroughton/app/App.java
+++ src/main/java/com/sambroughton/app/App.java
@@ -21,11 +21,17 @@ public class App {
             Statement stm = conn.createStatement();
             
             // SQL query
-            ResultSet rs = stm.executeQuery("select * from department");
+            ResultSet rs = stm.executeQuery("SELECT department.department_description, position.pay_type, employee.education_level "
+                                          + "FROM department "
+                                          + "INNER JOIN employee ON department.department_id=employee.department_id "
+                                          + "INNER JOIN position ON employee.position_id=position.position_id "
+                                          + "WHERE department_description='Store Management' "
+                                          + "AND pay_type='Monthly' "
+                                          + "AND education_level='Graduate Degree';");
             
             // Process rs
             while (rs.next()) {
-                System.out.println(rs.getString("department_id") + ", " + rs.getString("department_description"));
+                System.out.println(rs.getString("department_description") + ", "+ rs.getString("pay_type") + ", " + rs.getString("education_level"));
             }
             
         } catch (Exception e) {
@@ -33,3 +39,8 @@ public class App {
         }
     }
 }
+/*
+SELECT foodmart.department.department_description, foodmart.employee.education_level
+FROM foodmart.department
+INNER JOIN foodmart.employee ON foodmart.department.department_id=foodmart.employee.department_id;
+*/
