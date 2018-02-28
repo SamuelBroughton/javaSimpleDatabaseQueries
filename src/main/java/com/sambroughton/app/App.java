6ec8c09453f3d8b4332b417e6b6bda95aefdea81
sambroughton <sjbroughton1@sheffield.ac.uk>
2018-02-28 01:11:35 +0000

    Database connected, able to execute simple query

--- src/main/java/com/sambroughton/app/App.java
+++ src/main/java/com/sambroughton/app/App.java
@@ -1,13 +1,35 @@
 package com.sambroughton.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.*;
+
+public class App {
+    
+    public static void main( String[] args ) throws SQLException {        
+        
+        String URL = "jdbc:mysql://localhost:3306/foodmart";
+        String USER = "root";
+        String PASSWORD = "84Rts5-G.";
+                
+        
+        try {
+            // Connecting to database
+            System.out.println("Connecting to database. . .");
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected!");
+            
+            // Creating statement
+            Statement stm = conn.createStatement();
+            
+            // SQL query
+            ResultSet rs = stm.executeQuery("select * from department");
+            
+            // Process rs
+            while (rs.next()) {
+                System.out.println(rs.getString("department_id") + ", " + rs.getString("department_description"));
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
