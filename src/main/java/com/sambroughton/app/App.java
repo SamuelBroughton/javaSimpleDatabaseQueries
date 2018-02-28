371731e2fe8e80240bb29a443a10e1147a786491
sambroughton <sjbroughton1@sheffield.ac.uk>
2018-02-28 02:09:01 +0000

    Command line input implemented

--- src/main/java/com/sambroughton/app/App.java
+++ src/main/java/com/sambroughton/app/App.java
@@ -1,11 +1,17 @@
 package com.sambroughton.app;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.util.Scanner;
 
 public class App {
     
     public static void main( String[] args ) throws SQLException {        
         
+        Scanner scanner = new Scanner(System.in);
         String URL = "jdbc:mysql://localhost:3306/foodmart";
         String USER = "root";
         String PASSWORD = "84Rts5-G.";
@@ -14,27 +20,39 @@ public class App {
         try {
             // Connecting to database
             System.out.println("Connecting to database. . .");
-            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            Connection conn = (Connection)DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connected!");
             
+            // User input
+            System.out.print("Department name: ");
+            String deptName = scanner.nextLine();
+            System.out.print("Pay type: ");
+            String payType = scanner.nextLine();
+            System.out.print("Education: ");
+            String educationLevel = scanner.nextLine();
+            
             // Creating statement
-            Statement stm = conn.createStatement();
+            Statement stm = (Statement)conn.createStatement();
             
             // SQL query
             ResultSet rs = stm.executeQuery("SELECT department.department_description, position.pay_type, employee.education_level "
                                           + "FROM department "
                                           + "INNER JOIN employee ON department.department_id=employee.department_id "
                                           + "INNER JOIN position ON employee.position_id=position.position_id "
-                                          + "WHERE department_description='Store Management' "
-                                          + "AND pay_type='Monthly' "
-                                          + "AND education_level='Graduate Degree';");
+                                          + "WHERE department_description='" + deptName + "' "
+                                          + "AND pay_type='" + payType + "' "
+                                          + "AND education_level='" + educationLevel + "';");
             
             // Process rs
+            System.out.println();
+            System.out.println("Results of query:");
+            System.out.println();
             while (rs.next()) {
-                System.out.println(rs.getString("department_description") + ", "+ rs.getString("pay_type") + ", " + rs.getString("education_level"));
+                System.out.println(rs.getString("department_description") + " | "+ rs.getString("pay_type") + " | " + rs.getString("education_level"));
             }
             
         } catch (Exception e) {
+            System.out.println("FAILED:");
             e.printStackTrace();
         }
     }
